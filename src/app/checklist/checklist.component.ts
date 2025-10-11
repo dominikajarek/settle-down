1c7621b418dac5ae02300eb60d261a92d133fa4d
Dominika Jarek <dominikamarta.jarek@gmail.com>
2025-10-11 19:34:46 +0200

    Add checklist, add styling, add local storage settings

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,10 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { afterNextRender, ChangeDetectionStrategy, Component } from '@angular/core';
+import checklist from '../shared/models/checklist/checklist.json';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checklist',
-  imports: [],
+  imports: [FormsModule, CardModule, CheckboxModule],
   templateUrl: './checklist.component.html',
   styleUrl: './checklist.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChecklistComponent {}
+export class ChecklistComponent {
+  protected checklist = checklist;
+  protected storedData = localStorage.getItem('checklist');
+  protected jsonData = this.storedData ? JSON.parse(this.storedData) : null;
+
+  constructor() {
+    afterNextRender(() => {
+      if (this.checklist) {
+        this.checklist = this.jsonData;
+      }
+    });
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('checklist', JSON.stringify(this.checklist));
+  }
+}
